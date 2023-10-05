b36ab903603597c3aa1815b59bf9882b927fb575
sebas <srr1767@gmail.com>
2023-10-04 23:28:08 -0500

    Initial commit

---
+++ juego/bolos.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
+from typing import Optional
+
+@dataclass
+class Roll:
+    pins: int
+
+
+@dataclass
+class Frame(ABC):
+    rolls: list[Roll] = field(default_factory=list)
+
+    def open_frame(self) -> int:
+        return sum(roll.pins for roll in self.rolls)
+
+    def is_strike(self) -> bool:
+        return self.rolls[0].pins == 10
+
+    def is_spare(self) -> bool:
+        return self.rolls[0].pins + self.rolls[1].pins == 10
+
+    @abstractmethod
+    def add_roll(self, pins: int):
+        raise NotImplementedError
+
+    @abstractmethod
+    def score(self) -> int:
+        raise NotImplementedError
+
+
+class NormalFrame(Frame):
+    def __init__(self):
+        super().__init__()
+
+
+class TenthFrame(Frame):
+    pass
+
+
+class Game:
+    FRAMES_TOTALES = 10
